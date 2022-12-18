6cdeb3849b9225658d6431ae6ca6137251d4fb1e
Taylor Smith <tsmith@tsmithcreative.com>
2022-12-18 11:04:52 -0600

    Uhhh if you untar a tarball on root, you will brick your OS. Fair warning.

--- dist/home/paperframe/update.sh
+++ dist/home/paperframe/update.sh
@@ -3,12 +3,24 @@
 # @TODO: This will get the newest file by time, not by tarball version.
 PACKAGE=$(ls -Art paperframe*.tar.gz | tail -n 1)
 
-sudo tar \
+rm -rf /tmp/paperframe-install
+mkdir /tmp/paperframe-install
+
+tar \
   --no-overwrite-dir --no-same-owner --no-same-permissions \
-  -xzf $PACKAGE -C /
+  -C /tmp/paperframe-install -xzf $PACKAGE
+
+rm $PACKAGE
+
+cd /tmp/paperframe-install
+
+# @TODO: This will fail for any  nonexistant parent directory:
+sudo find * -type f -exec mv {} /{} \;
 
 sudo systemctl daemon-reload
-
+sudo systemctl enable paperframe
 sudo systemctl restart paperframe
 
-# rm $PACKAGE
+cd ~
+
+rm -r /tmp/paperframe-install
